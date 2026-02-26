4439015e7b1ccb736027d655d2a9fbc5b98c0b23
Harsh Jain <h.jain@amd.com>
2026-02-26 22:35:27 +0530

    crypto: asu: Add crypto hash driver
    Add support for following Hash algorithms
    SHA-256, SHA-384, SHA-512, SHA3-256, SHA3-384, SHA3-512
    
    Signed-off-by: Harsh Jain <h.jain@amd.com>
    Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>

---
+++ core/drivers/crypto/asu_driver/sub.mk
@@ -0,0 +1,9 @@
+ # SPDX-License-Identifier: BSD-2-Clause
+ #
+ # Copyright (c) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+ #
+ #
+
+incdirs-y += ../../amd/asu/include/
+
+srcs-$(CFG_AMD_ASU_HASH) += asu_hash.c

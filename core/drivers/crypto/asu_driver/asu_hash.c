ef7b178c2769e1b0612a82d817e57831e193b204
Harish Ediga <harish.ediga@amd.com>
2026-04-07 16:13:30 +0530

    crypto: asu: send hash address in request buffer for digest
    Update driver to read digest from hash address sent to firmware
    instead of response buffer
    
    Fixes: 74ddb42edbe0ea886661c62476bac147ad3141aa ("crypto: asu: Add
    crypto hash driver")
    
    Signed-off-by: Harish Ediga <harish.ediga@amd.com>

--- core/drivers/crypto/asu_driver/asu_hash.c
+++ core/drivers/crypto/asu_driver/asu_hash.c
@@ -67,11 +67,6 @@ struct asu_hash_ctx {
 	uint8_t module;
 };
 
-struct asu_hash_cbctx {
-	uint8_t *digest;
-	size_t len;
-};
-
 static const struct crypto_hash_ops asu_hash_ops;
 static struct asu_shadev *asu_shadev;
 static struct asu_hash_ctx *to_hash_ctx(struct crypto_hash_ctx *ctx);
@@ -242,18 +237,6 @@ static TEE_Result asu_hash_do_update(struct crypto_hash_ctx *ctx,
 	return asu_hash_update(asu_hashctx, (uint8_t *)data, len);
 }
 
-static TEE_Result asu_hash_cb(void *cbrefptr, struct asu_resp_buf *resp_buf)
-{
-	struct asu_hash_cbctx *cbctx = NULL;
-	uint8_t *src_addr = NULL;
-
-	cbctx = cbrefptr;
-	src_addr = (uint8_t *)&resp_buf->arg[ASU_RESPONSE_BUFF_ADDR_INDEX];
-	memcpy(cbctx->digest, src_addr, cbctx->len);
-
-	return TEE_SUCCESS;
-}
-
 /**
  * asu_hash_final() - Send final request to engine.
  * @asu_hashctx:Request private hash context
@@ -271,22 +254,24 @@ static TEE_Result asu_hash_final(struct asu_hash_ctx *asu_hashctx,
 	TEE_Result ret = TEE_SUCCESS;
 	struct asu_sha_op_cmd op = {};
 	struct asu_client_params *cparam = NULL;
-	struct asu_hash_cbctx cbctx = {};
+	uint32_t *dma_digest = NULL;
 
 	if (!digest || len == 0)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	cbctx.digest = digest;
-	cbctx.len = len;
 	cparam = &asu_hashctx->cparam;
 	cparam->priority = ASU_PRIORITY_HIGH;
-	cparam->cbptr = &cbctx;
-	cparam->cbhandler = asu_hash_cb;
+	cparam->cbhandler = NULL;
+	dma_digest = memalign(64, ASU_SHAKE_256_MAX_HASH_LEN);
+	if (!dma_digest) {
+		EMSG("Failed to allocate DMA buffer for hash digest");
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
 
 	/* Inputs of SHA request */
 	op.dataaddr = 0;
 	op.datasize = 0;
-	op.hashaddr = UINT64_MAX;
 	op.hashbufsize = len;
 	if (asu_hashctx->shamode == ASU_SHA_MODE_SHA256)
 		op.hashbufsize = ASU_SHA_256_HASH_LEN;
@@ -298,9 +283,18 @@ static TEE_Result asu_hash_final(struct asu_hash_ctx *asu_hashctx,
 	op.shamode = asu_hashctx->shamode;
 	op.islast = 1;
 	op.opflags = ASU_SHA_FINISH | asu_hashctx->shastart;
+	op.hashaddr = virt_to_phys(dma_digest);
+	cache_operation(TEE_CACHEFLUSH, dma_digest, op.hashbufsize);
 	ret = asu_sha_op(asu_hashctx, &op, asu_hashctx->module);
-	cache_operation(TEE_CACHEFLUSH, digest, op.hashbufsize);
+	if (ret) {
+		EMSG("SHA final operation failed");
+		goto out;
+	}
+	cache_operation(TEE_CACHEINVALIDATE, dma_digest, op.hashbufsize);
+	memcpy(digest, dma_digest, op.hashbufsize);
 
+out:
+	free(dma_digest);
 	return ret;
 }
 

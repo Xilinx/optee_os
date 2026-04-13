Xilinx/optee_os

commit 74ddb42edbe0ea886661c62476bac147ad3141aa

crypto: asu: Add crypto hash driver
Add support for following Hash algorithms
SHA-256, SHA-384, SHA-512, SHA3-256, SHA3-384, SHA3-512

Signed-off-by: Harsh Jain <h.jain@amd.com>
Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/asu_driver/asu_hash.c
@@ -1,0 +1,448 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+ *
+ */
+
+#include <assert.h>
+#include <drivers/amd/asu_client.h>
+#include <drvcrypt_hash.h>
+#include <initcall.h>
+#include <io.h>
+#include <kernel/mutex.h>
+#include <kernel/panic.h>
+#include <kernel/unwind.h>
+#include <mm/core_memprot.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <tee/cache.h>
+#include <trace.h>
+#include <util.h>
+
+#define ASU_SHA_OPERATION_CMD_ID		0U
+/* SHA modes */
+#define ASU_SHA_MODE_SHA256			0U
+#define ASU_SHA_MODE_SHA384			1U
+#define ASU_SHA_MODE_SHA512			2U
+#define ASU_SHA_MODE_SHAKE256			4U
+
+/* SHA operation mode */
+#define ASU_SHA_START				0x1U
+#define ASU_SHA_UPDATE				0x2U
+#define ASU_SHA_FINISH				0x4U
+
+/* SHA hash lengths */
+#define ASU_SHA_256_HASH_LEN			32U
+#define ASU_SHA_384_HASH_LEN			48U
+#define ASU_SHA_512_HASH_LEN			64U
+#define ASU_SHAKE_256_HASH_LEN			32U
+#define ASU_SHAKE_256_MAX_HASH_LEN		136U
+#define ASU_DATA_CHUNK_LEN			4096U
+
+struct asu_shadev {
+	bool sha2_available;
+	bool sha3_available;
+	/* Control access to engine*/
+	struct mutex engine_lock;
+};
+
+struct asu_sha_op_cmd {
+	uint64_t dataaddr;
+	uint64_t hashaddr;
+	uint32_t datasize;
+	uint32_t hashbufsize;
+	uint8_t shamode;
+	uint8_t islast;
+	uint8_t opflags;
+	uint8_t shakereserved;
+};
+
+struct asu_hash_ctx {
+	struct crypto_hash_ctx hash_ctx; /* Crypto Hash API context */
+	struct asu_client_params cparam;
+	uint32_t shamode;
+	uint32_t shastart;
+	uint8_t uniqueid;
+	uint8_t module;
+};
+
+struct asu_hash_cbctx {
+	uint8_t *digest;
+	size_t len;
+};
+
+static const struct crypto_hash_ops asu_hash_ops;
+static struct asu_shadev *asu_shadev;
+static struct asu_hash_ctx *to_hash_ctx(struct crypto_hash_ctx *ctx);
+
+/**
+ * asu_hash_get_alg() - Get fw engine module ID and Hash mode.
+ * @algo:	TEE algo type.
+ * @module:	Engine module ID
+ * @mode:	Hash operation mode
+ * Map TEE algo type to fw module ID amd mode.
+ *
+ * Return: TEE_SUCCESS or TEE_ERROR_NOT_IMPLEMENTED
+ */
+
+static TEE_Result asu_hash_get_alg(uint32_t algo,
+				   uint32_t *module,
+				   uint32_t *mode)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	switch (algo) {
+	case TEE_ALG_SHA256:
+		*module = ASU_MODULE_SHA2_ID;
+		*mode = ASU_SHA_MODE_SHA256;
+		break;
+	case TEE_ALG_SHA384:
+		*module = ASU_MODULE_SHA2_ID;
+		*mode = ASU_SHA_MODE_SHA384;
+		break;
+	case TEE_ALG_SHA512:
+		*module = ASU_MODULE_SHA2_ID;
+		*mode = ASU_SHA_MODE_SHA512;
+		break;
+	case TEE_ALG_SHA3_256:
+		*module = ASU_MODULE_SHA3_ID;
+		*mode = ASU_SHA_MODE_SHA256;
+		break;
+	case TEE_ALG_SHA3_384:
+		*module = ASU_MODULE_SHA3_ID;
+		*mode = ASU_SHA_MODE_SHA384;
+		break;
+	case TEE_ALG_SHA3_512:
+		*module = ASU_MODULE_SHA3_ID;
+		*mode = ASU_SHA_MODE_SHA512;
+		break;
+	default:
+		ret = TEE_ERROR_NOT_IMPLEMENTED;
+		break;
+	}
+
+	return ret;
+}
+
+/**
+ * asu_hash_initialize() - Initialize private asu_hash_ctx for hash operation.
+ * @ctx: crypto context used by the crypto_hash_*() functions
+ * Initialize hash operation request
+ *
+ * Return: TEE_SUCCESS or TEE_ERROR_BAD_PARAMETERS
+ */
+
+static TEE_Result asu_hash_initialize(struct crypto_hash_ctx *ctx)
+{
+	to_hash_ctx(ctx)->shastart = ASU_SHA_START;
+
+	return TEE_SUCCESS;
+}
+
+/**
+ * asu_sha_op() - Perform hash operation.
+ * @asu_hashctx:Request private hash context
+ * @op:		asu_sha_op_cmd parameters for fw engine
+ * @module:	Engine module ID
+ * @data:	Output digest received from engine
+ * Create request header, send and wait for result
+ * from engine.
+ *
+ * Return: TEE_SUCCESS or TEE_ERROR_GENERIC
+ */
+
+static TEE_Result asu_sha_op(struct asu_hash_ctx *asu_hashctx,
+			     struct asu_sha_op_cmd *op,
+			     uint8_t module)
+{
+	TEE_Result ret = TEE_SUCCESS;
+	uint32_t header = 0;
+	uint32_t status = 0;
+
+	header = asu_create_header(ASU_SHA_OPERATION_CMD_ID,
+				   asu_hashctx->uniqueid, module, 0U);
+	ret = asu_update_queue_buffer_n_send_ipi(&asu_hashctx->cparam, op,
+						 sizeof(*op), header,
+						 &status);
+	if (status) {
+		EMSG("FW error 0x%x\n", status);
+		ret = TEE_ERROR_GENERIC;
+	}
+
+	return ret;
+}
+
+/**
+ * asu_hash_update() - Send update request to engine.
+ * @asu_hashctx:Request private hash context
+ * @data:	Input data buffer
+ * @len:	Size of data buffer
+ * Send update request to engine
+ * from engine.
+ *
+ * Return: TEE_SUCCESS or TEE_ERROR_GENERIC
+ */
+
+static TEE_Result asu_hash_update(struct asu_hash_ctx *asu_hashctx,
+				  uint8_t *data, size_t len)
+{
+	TEE_Result ret = TEE_SUCCESS;
+	struct asu_sha_op_cmd op = {};
+	struct asu_client_params *cparam = NULL;
+	uint32_t remaining = 0;
+
+	/* Inputs of client request */
+	cparam = &asu_hashctx->cparam;
+	cparam->priority = ASU_PRIORITY_HIGH;
+	cparam->cbhandler = NULL;
+
+	/* Inputs of SHA request */
+	cache_operation(TEE_CACHEFLUSH, data, len);
+	op.hashaddr = 0;
+	op.hashbufsize = 0;
+	op.shamode = asu_hashctx->shamode;
+	op.islast = 0;
+	remaining = len;
+	while (remaining) {
+		op.datasize = MIN(remaining, ASU_DATA_CHUNK_LEN);
+		op.opflags = ASU_SHA_UPDATE | asu_hashctx->shastart;
+		op.dataaddr = virt_to_phys(data);
+		remaining -= op.datasize;
+		data += op.datasize;
+		ret = asu_sha_op(asu_hashctx, &op, asu_hashctx->module);
+		if (ret)
+			break;
+		asu_hashctx->shastart = 0;
+	}
+
+	return ret;
+}
+
+static TEE_Result asu_hash_do_update(struct crypto_hash_ctx *ctx,
+				     const uint8_t *data, size_t len)
+{
+	struct asu_hash_ctx *asu_hashctx = NULL;
+
+	if (!len) {
+		DMSG("This is 0 len task, skip");
+		return TEE_SUCCESS;
+	}
+
+	if (!data && len) {
+		EMSG("Invalid input parameters");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	asu_hashctx = to_hash_ctx(ctx);
+	if (asu_hashctx->uniqueid == ASU_UNIQUE_ID_MAX)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	return asu_hash_update(asu_hashctx, (uint8_t *)data, len);
+}
+
+static TEE_Result asu_hash_cb(void *cbrefptr, struct asu_resp_buf *resp_buf)
+{
+	struct asu_hash_cbctx *cbctx = NULL;
+	uint8_t *src_addr = NULL;
+
+	cbctx = cbrefptr;
+	src_addr = (uint8_t *)&resp_buf->arg[ASU_RESPONSE_BUFF_ADDR_INDEX];
+	memcpy(cbctx->digest, src_addr, cbctx->len);
+
+	return TEE_SUCCESS;
+}
+
+/**
+ * asu_hash_final() - Send final request to engine.
+ * @asu_hashctx:Request private hash context
+ * @digest:	Output digest buffer
+ * @len:	Size of digest buffer
+ *
+ * Send final request to engine and populate digest result
+ *
+ * Return: TEE_SUCCESS, TEE_ERROR_BAD_PARAMETERS or TEE_ERROR_GENERIC
+ */
+
+static TEE_Result asu_hash_final(struct asu_hash_ctx *asu_hashctx,
+				 uint8_t *digest, size_t len)
+{
+	TEE_Result ret = TEE_SUCCESS;
+	struct asu_sha_op_cmd op = {};
+	struct asu_client_params *cparam = NULL;
+	struct asu_hash_cbctx cbctx = {};
+
+	if (!digest || len == 0)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	cbctx.digest = digest;
+	cbctx.len = len;
+	cparam = &asu_hashctx->cparam;
+	cparam->priority = ASU_PRIORITY_HIGH;
+	cparam->cbptr = &cbctx;
+	cparam->cbhandler = asu_hash_cb;
+
+	/* Inputs of SHA request */
+	op.dataaddr = 0;
+	op.datasize = 0;
+	op.hashaddr = UINT64_MAX;
+	op.hashbufsize = len;
+	if (asu_hashctx->shamode == ASU_SHA_MODE_SHA256)
+		op.hashbufsize = ASU_SHA_256_HASH_LEN;
+	else if (asu_hashctx->shamode == ASU_SHA_MODE_SHA384)
+		op.hashbufsize = ASU_SHA_384_HASH_LEN;
+	else if (asu_hashctx->shamode == ASU_SHA_MODE_SHA512)
+		op.hashbufsize = ASU_SHA_512_HASH_LEN;
+
+	op.shamode = asu_hashctx->shamode;
+	op.islast = 1;
+	op.opflags = ASU_SHA_FINISH | asu_hashctx->shastart;
+	ret = asu_sha_op(asu_hashctx, &op, asu_hashctx->module);
+	cache_operation(TEE_CACHEFLUSH, digest, op.hashbufsize);
+
+	return ret;
+}
+
+static TEE_Result asu_hash_do_final(struct crypto_hash_ctx *ctx,
+				    uint8_t *digest, size_t len)
+{
+	struct asu_hash_ctx *asu_hashctx = NULL;
+
+	asu_hashctx = to_hash_ctx(ctx);
+
+	return asu_hash_final(asu_hashctx, digest, len);
+}
+
+/**
+ * asu_hash_ctx_free() - Free Private context.
+ * @crypto_hash_ctx: crypto context used by the crypto_hash_*() functions
+ * Release crypto engine and free private context memory.
+ *
+ * Return: void
+ */
+
+static void asu_hash_ctx_free(struct crypto_hash_ctx *ctx)
+{
+	struct asu_hash_ctx *asu_hashctx = NULL;
+
+	asu_hashctx = to_hash_ctx(ctx);
+	asu_free_unique_id(asu_hashctx->uniqueid);
+	asu_hashctx->uniqueid = ASU_UNIQUE_ID_MAX;
+	mutex_lock(&asu_shadev->engine_lock);
+	if (asu_hashctx->module == ASU_MODULE_SHA2_ID) {
+		assert(!asu_shadev->sha2_available);
+		asu_shadev->sha2_available = true;
+	} else if (asu_hashctx->module == ASU_MODULE_SHA3_ID) {
+		assert(!asu_shadev->sha3_available);
+		asu_shadev->sha3_available = true;
+	}
+	mutex_unlock(&asu_shadev->engine_lock);
+	free(asu_hashctx);
+}
+
+static const struct crypto_hash_ops asu_hash_ops = {
+	.init = asu_hash_initialize,
+	.update = asu_hash_do_update,
+	.final = asu_hash_do_final,
+	.free_ctx = asu_hash_ctx_free,
+	/*
+	 * Current engine does not support partial state copy operation.
+	 */
+	.copy_state = NULL,
+};
+
+/*
+ * Returns the reference to the driver context
+ *
+ * @ctx  API Context
+ */
+static struct asu_hash_ctx *to_hash_ctx(struct crypto_hash_ctx *ctx)
+{
+	assert(ctx && ctx->ops == &asu_hash_ops);
+
+	return container_of(ctx, struct asu_hash_ctx, hash_ctx);
+}
+
+/**
+ * asu_hash_ctx_allocate() - Allocate Private context.
+ * @crypto_hash_ctx: crypto context used by the crypto_hash_*() functions
+ * @algo:	TEE algo type.
+ * Grab crypto engine and free private context memory.
+ *
+ * Return: TEE_SUCCESS, TEE_ERROR_BAD_PARAMETERS or TEE_ERROR_OUT_OF_MEMORY
+ */
+
+static TEE_Result asu_hash_ctx_allocate(struct crypto_hash_ctx **ctx,
+					uint32_t algo)
+{
+	struct asu_hash_ctx *asu_hashctx = NULL;
+	uint32_t module = 0;
+	uint32_t shamode = 0;
+	TEE_Result ret = TEE_SUCCESS;
+
+	ret = asu_hash_get_alg(algo, &module, &shamode);
+	if (ret)
+		return ret;
+	mutex_lock(&asu_shadev->engine_lock);
+	if (module == ASU_MODULE_SHA2_ID && asu_shadev->sha2_available) {
+		asu_shadev->sha2_available = false;
+	} else if (module == ASU_MODULE_SHA3_ID && asu_shadev->sha3_available) {
+		asu_shadev->sha3_available = false;
+	} else {
+		mutex_unlock(&asu_shadev->engine_lock);
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+	mutex_unlock(&asu_shadev->engine_lock);
+
+	asu_hashctx = calloc(1, sizeof(*asu_hashctx));
+	if (!asu_hashctx) {
+		EMSG("Fail to alloc hash");
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto free_dev_mem;
+	}
+
+	asu_hashctx->module = module;
+	asu_hashctx->shamode = shamode;
+	asu_hashctx->uniqueid = asu_alloc_unique_id();
+
+	if (asu_hashctx->uniqueid == ASU_UNIQUE_ID_MAX) {
+		EMSG("Fail to get unique ID");
+		ret = TEE_ERROR_BAD_PARAMETERS;
+		goto free_dev_mem;
+	}
+	asu_hashctx->hash_ctx.ops = &asu_hash_ops;
+	*ctx = &asu_hashctx->hash_ctx;
+
+	return ret;
+
+free_dev_mem:
+	mutex_lock(&asu_shadev->engine_lock);
+	if (asu_hashctx->module == ASU_MODULE_SHA2_ID &&
+	    !asu_shadev->sha2_available)
+		asu_shadev->sha2_available = true;
+	else if (asu_hashctx->module == ASU_MODULE_SHA3_ID &&
+		 !asu_shadev->sha3_available)
+		asu_shadev->sha3_available = true;
+	mutex_unlock(&asu_shadev->engine_lock);
+
+	if (asu_hashctx)
+		free(asu_hashctx);
+
+	return ret;
+}
+
+static TEE_Result asu_hash_init(void)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	asu_shadev = calloc(1, sizeof(*asu_shadev));
+	mutex_init(&asu_shadev->engine_lock);
+	asu_shadev->sha2_available = true;
+	asu_shadev->sha3_available = true;
+	ret = drvcrypt_register_hash(&asu_hash_ctx_allocate);
+	if (ret)
+		EMSG("ASU hash register to crypto fail ret=%#"PRIx32, ret);
+
+	return ret;
+}
+driver_init(asu_hash_init);

core/arch/arm/plat-versal2/conf.mk
@@ -64,6 +64,15 @@ CFG_AMD_PS_GPIO ?= n
 # AMD ASU Specific configs
 CFG_AMD_ASU_SUPPORT ?= y
 CFG_AMD_APU_LCL_IPI_ID ?= 0x0004
+# Current HASH engine does not support partial state copy operation. Any
+# operation from REE involving state copy can crash the OS when driver
+# is enabled.
+CFG_AMD_ASU_HASH ?= n
+ifeq ($(CFG_AMD_ASU_HASH),y)
+$(warning WARNING: ASU HASH engine do not support partial state copy operations)
+$(warning WARNING: Any attempt by the REE to perform a state copy operation \
+  will result in a crash of the TEE.)
+endif
 
 ifeq ($(CFG_AMD_PS_GPIO),y)
 $(call force,CFG_MAP_EXT_DT_SECURE,y)

core/drivers/crypto/asu_driver/crypto.mk
@@ -1,0 +1,13 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+#
+#
+
+ifeq ($(CFG_AMD_ASU_SUPPORT),y)
+# Enable the crypto driver
+$(call force,CFG_CRYPTO_DRIVER,y)
+CFG_CRYPTO_DRIVER_DEBUG ?= 0
+$(call force,CFG_CRYPTO_DRV_HASH,y)
+
+endif

core/drivers/crypto/asu_driver/sub.mk
@@ -1,0 +1,8 @@
+ # SPDX-License-Identifier: BSD-2-Clause
+ #
+ # Copyright (c) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+ #
+ #
+
+
+srcs-$(CFG_AMD_ASU_HASH) += asu_hash.c

core/drivers/crypto/sub.mk
@@ -12,6 +12,8 @@ subdirs-$(CFG_ASPEED_CRYPTO_DRIVER) += aspeed
 
 subdirs-$(CFG_VERSAL_CRYPTO_DRIVER) += versal
 
+subdirs-$(CFG_AMD_ASU_SUPPORT) += asu_driver
+
 subdirs-$(CFG_HISILICON_CRYPTO_DRIVER) += hisilicon
 
 subdirs-$(CFG_IMX_ELE) += ele

commit 6ef3cdc29b473733061e3439175a6887f41aa07b

crypto: asu: fix command header length and race condition
- Updated asu_create_header() to pass command length in words
  (sizeof(*cp)/sizeof(uint32_t)) instead of zero, to align with
  ASUFW requirements for proper command parsing.
- Multiple crypto drivers can submit request to shared buffer.
  Make sure all queue parameters are update in spin_lock.

Fixes: Fixes: 74ddb42 ("crypto: asu: Add crypto hash driver")

Signed-off-by: Harish Ediga <harish.ediga@amd.com>

core/drivers/amd/asu/asu_main.c
@@ -41,6 +41,8 @@
 struct asu_client {
 	struct asu_channel_memory *chnl_memptr;
 	uint32_t is_ready;
+	uint32_t p0_last_index;
+	uint32_t p1_last_index;
 	unsigned int slock;          /* chnl_memptr spin lock */
 	void *global_ctrl;
 	void *doorbell;
@@ -188,40 +190,33 @@ void asu_free_unique_id(uint8_t uniqueid)
 }
 
 /*
- * get_free_index() - Find a free buffer index in the specified priority queue
- * @priority: Priority level (ASU_PRIORITY_HIGH or ASU_PRIORITY_LOW)
+ * get_free_index() - Find a free buffer index in the specified queue
+ * @qptr: Pointer to the channel queue structure to search
+ * @last_index: Pointer to track the starting search index for
+ *              round-robin allocation
  *
- * Searches for an available buffer slot in either the high priority (P0) or
- * low priority (P1) channel queue. Updates the next free index pointer.
+ * Searches for an available buffer slot in the specified channel queue.
+ * Performs a circular search of ASU_MAX_BUFFERS slots starting from the
+ * provided index, wrapping around if necessary.
  *
  * Return: Buffer index (0 to ASU_MAX_BUFFERS-1) or ASU_MAX_BUFFERS if queue
  *	   is full
  */
-static uint8_t get_free_index(uint8_t priority)
+static uint8_t get_free_index(struct asu_channel_queue *qptr,
+			      uint32_t *last_index)
 {
-	struct asu_channel_queue *qptr = NULL;
-	uint8_t index = 0;
-	uint32_t state = 0;
+	uint8_t index = *last_index;
+	uint8_t iterations = 0;
 
-	state = cpu_spin_lock_xsave(&asu->slock);
-
-	if (priority == ASU_PRIORITY_HIGH)
-		qptr = &asu->chnl_memptr->p0_chnl_q;
-	else
-		qptr = &asu->chnl_memptr->p1_chnl_q;
-
-	while (index < ASU_MAX_BUFFERS) {
-		if (qptr->queue_bufs[index].reqbufstatus == 0U ||
-		    qptr->queue_bufs[index].respbufstatus == 0U)
+	while (iterations < ASU_MAX_BUFFERS) {
+		index = (index + 1) % ASU_MAX_BUFFERS;
+		if (qptr->queue_bufs[index].reqbufstatus == 0U)
 			break;
-
-		index++;
+		iterations++;
 	}
 
-	if (index < ASU_MAX_BUFFERS)
-		qptr->queue_bufs[index].reqbufstatus = ASU_COMMAND_IS_PRESENT;
-
-	cpu_spin_unlock_xrestore(&asu->slock, state);
+	if (iterations == ASU_MAX_BUFFERS)
+		return ASU_MAX_BUFFERS;
 
 	return index;
 }
@@ -273,10 +268,12 @@ TEE_Result asu_update_queue_buffer_n_send_ipi(struct asu_client_params *param,
 {
 	TEE_Result ret = TEE_ERROR_GENERIC;
 	uint8_t freeindex = 0;
+	uint32_t *last_index;
+	uint32_t state = 0;
 	struct asu_channel_queue_buf *bufptr = NULL;
 	struct asu_channel_queue *qptr = NULL;
 
-	if (!param) {
+	if (!param || !status) {
 		EMSG("Invalid parameters provided");
 		return TEE_ERROR_BAD_PARAMETERS;
 	}
@@ -286,28 +283,34 @@ TEE_Result asu_update_queue_buffer_n_send_ipi(struct asu_client_params *param,
 		return TEE_ERROR_BAD_STATE;
 	}
 
-	freeindex = get_free_index(param->priority);
+	if (param->priority == ASU_PRIORITY_HIGH) {
+		qptr = &asu->chnl_memptr->p0_chnl_q;
+		last_index = &asu->p0_last_index;
+	} else {
+		qptr = &asu->chnl_memptr->p1_chnl_q;
+		last_index = &asu->p1_last_index;
+	}
+
+	state = cpu_spin_lock_xsave(&asu->slock);
+	freeindex = get_free_index(qptr, last_index);
 	if (freeindex == ASU_MAX_BUFFERS) {
 		EMSG("ASU buffers full");
+		cpu_spin_unlock_xrestore(&asu->slock, state);
 		return TEE_ERROR_SHORT_BUFFER;
 	}
 
-	if (param->priority == ASU_PRIORITY_HIGH) {
-		bufptr = &asu->chnl_memptr->p0_chnl_q.queue_bufs[freeindex];
-		qptr = &asu->chnl_memptr->p0_chnl_q;
-	} else {
-		bufptr = &asu->chnl_memptr->p1_chnl_q.queue_bufs[freeindex];
-		qptr = &asu->chnl_memptr->p1_chnl_q;
-	}
-
+	bufptr = &qptr->queue_bufs[freeindex];
 	bufptr->req.header = header;
 	if (req_buffer && size != 0U)
 		memcpy(bufptr->req.arg, req_buffer, size);
 
 	bufptr->respbufstatus = 0;
-
+	qptr->queue_bufs[freeindex].reqbufstatus = ASU_COMMAND_IS_PRESENT;
+	*last_index = freeindex;
 	qptr->cmd_is_present = true;
 	qptr->req_sent++;
+	cpu_spin_unlock_xrestore(&asu->slock, state);
+
 	ret = send_doorbell();
 	if (ret != TEE_SUCCESS) {
 		EMSG("Failed to communicate to ASU");
@@ -469,6 +472,8 @@ static TEE_Result asu_init(void)
 	asu->chnl_memptr = asu_shmem;
 	asu->is_ready = ASU_CLIENT_READY;
 	asu->slock = SPINLOCK_UNLOCK;
+	asu->p0_last_index = ASU_MAX_BUFFERS - 1;
+	asu->p1_last_index = ASU_MAX_BUFFERS - 1;
 
 	IMSG("ASU initialization complete");
 

core/drivers/crypto/asu_driver/asu_hash.c
@@ -161,7 +161,8 @@ static TEE_Result asu_sha_op(struct asu_hash_ctx *asu_hashctx,
 	uint32_t status = 0;
 
 	header = asu_create_header(ASU_SHA_OPERATION_CMD_ID,
-				   asu_hashctx->uniqueid, module, 0U);
+				   asu_hashctx->uniqueid, module,
+				   sizeof(*op) / sizeof(uint32_t));
 	ret = asu_update_queue_buffer_n_send_ipi(&asu_hashctx->cparam, op,
 						 sizeof(*op), header,
 						 &status);